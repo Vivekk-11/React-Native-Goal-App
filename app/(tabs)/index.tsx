00e867da70d3dd79b8d951df3201bb865ecf553b
vivek <chimnanivivek14@gmail.com>
2024-05-14 12:15:59 +0530

    layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,10 +4,10 @@ export default function HomeScreen() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Your course goal!" />
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -17,18 +17,27 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+    paddingHorizontal: 16,
     backgroundColor: "white",
-    height: "100%",
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: "80%",
+    width: "70%",
     padding: 8,
     margin: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
